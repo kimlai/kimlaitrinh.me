bde918df8f975fb8b527eae6ddc49ec7155fe268
kimlai <hello@kimlaitrinh.me>
2020-06-30 00:55:12 +0200

    Get rid of tailwind css
    I prefer my CSS in its own stylesheet :)

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,8 @@
-const purgecss = require("@fullhuman/postcss-purgecss")({
-  content: ["./**/*.html", "./**/*.njk", "./**/*.md"],
-  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
-});
 const cssnano = require("cssnano")({ preset: "default" });
 
 module.exports = {
   plugins: [
-    require("tailwindcss"),
     require("autoprefixer"),
-    ...(process.env.NODE_ENV === "production" ? [purgecss] : []),
     ...(process.env.NODE_ENV === "production" ? [cssnano] : [])
   ]
 };
