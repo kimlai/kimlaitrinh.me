78a39a160df06b481894355f6815cfff1975736b
kimlai <hello@kimlaitrinh.me>
2020-06-29 23:44:32 +0200

    do not rely on tailwind for colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,5 @@
 module.exports = {
   corePlugins: {
     fontSize: false
-  },
-  theme: {
-    colors: {
-      white: "#fafafa",
-      black: "#050505",
-      "green-400": "#68D391"
-    }
   }
 };
